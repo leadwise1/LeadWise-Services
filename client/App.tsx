ad0d60acb1e222f1e056c26532119f65c889bd3f
LeadWise Foundation <sales@causevay.com>
2025-12-27 08:12:46 -0600

    Update App.tsx

--- client/App.tsx
+++ client/App.tsx
@@ -5,7 +5,7 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
 import Templates from "./pages/Templates";
-import Courses from "./pages/Courses";
+import Academy from "./pages/Academy"; // <-- NEW IMPORT NAME
 import Editor from "./pages/Editor";
 import NotFound from "./pages/NotFound";
 
@@ -20,7 +20,7 @@ const App = () => (
         <Routes>
           <Route path="/" element={<Index />} />
           <Route path="/templates" element={<Templates />} />
-          <Route path="/courses" element={<Courses />} />
+          <Route path="/courses" element={<Academy />} /> {/* <-- USE ACADEMY HERE */}
           <Route path="/editor" element={<Editor />} />
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
           <Route path="*" element={<NotFound />} />
