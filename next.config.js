358b4b9c742c445bb20f2f91de1233305d8a3127
LeadWise Foundation <sales@causevay.com>
2026-01-01 19:10:36 -0600

    Create next.config.js

---
+++ next.config.js
@@ -0,0 +1,13 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  async rewrites() {
+    return [
+      {
+        source: "/sitemap.xml",
+        destination: "/api/sitemap.xml",
+      },
+    ];
+  },
+};
+
+export default nextConfig;
