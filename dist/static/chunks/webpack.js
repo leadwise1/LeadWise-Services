3682c85baba5532607c0e53d3df5a93562a13546
Shanaz Lakhani <sales@causevay.com>
2026-01-13 19:39:26 -0600

    Cleanup: Remove unused services page and update homepage metadata

--- dist/static/chunks/webpack.js
+++ dist/static/chunks/webpack.js
@@ -180,7 +180,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "dbc8336a6e26dd53"; }
+/******/ 		__webpack_require__.h = function() { return "efe2cf7acd8432e0"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- dist/static/webpack/webpack.f3ea37130ad342fc.hot-update.js
+++ dist/static/webpack/webpack.f3ea37130ad342fc.hot-update.js
@@ -11,7 +11,7 @@ self["webpackHotUpdate_N_E"]("webpack",{},
 /******/ function(__webpack_require__) { // webpackRuntimeModules
 /******/ /* webpack/runtime/getFullHash */
 /******/ !function() {
-/******/ 	__webpack_require__.h = function() { return "dbc8336a6e26dd53"; }
+/******/ 	__webpack_require__.h = function() { return "efe2cf7acd8432e0"; }
 /******/ }();
 /******/ 
 /******/ }
